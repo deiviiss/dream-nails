e0cc2e8afb49a9e16ff2ce401be5f77a53b25fcd
David Hilera <david.hilera@hotmail.com>
2025-07-27 02:09:06 -0600

    fix: update contact address in ContactSection component

--- src/components/dream-nails/landing-page/ContactSection.tsx
+++ src/components/dream-nails/landing-page/ContactSection.tsx
@@ -20,7 +20,7 @@ export function ContactSection() {
     },
     {
       icon: <MapPin className="h-5 w-5 text-[#d47983]" />,
-      text: 'Calle José María Iglesias,  Campeche CP 24088'
+      text: 'And Nuevo León Mz 48 Lt 25 Fidel Velázquez,  Campeche CP 24088'
     }
   ]
 
